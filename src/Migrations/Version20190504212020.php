9fc487f3fd072b36d38040a200c224c395cea5b0
Dexodus2309 <daniilarosuk9@gmail.com>
2019-05-05 00:32:49 +0300

    Fix code-style bugs(uses)

--- src/Migrations/Version20190504212020.php
+++ src/Migrations/Version20190504212020.php
@@ -7,19 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190504212020 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function up(Schema $schema): void
     {
-        return '';
-    }
-
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE order_product (order_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_2530ADE68D9F6D38 (order_id), INDEX IDX_2530ADE64584665A (product_id), PRIMARY KEY(order_id, product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
@@ -28,9 +19,8 @@ final class Version20190504212020 extends AbstractMigration
         $this->addSql('DROP TABLE ord_product');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE ord_product (ord_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_99B86CA64584665A (product_id), INDEX IDX_99B86CA6E636D3F5 (ord_id), PRIMARY KEY(ord_id, product_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
