404566f854f72bf0eea3daccf317bb05a7cbdc3e
Dexodus2309 <daniilarosuk9@gmail.com>
2019-06-17 12:37:57 +0300

    Implement OrderCollectionInterface

---
+++ src/Model/OrderCollection.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Model;
+
+use App\Entity\Order;
+
+class OrderCollection implements OrderCollectionInterface
+{
+    /**
+     * @var Order[] $orders
+     */
+    private $orders;
+
+    /**
+     * @param Order $order
+     *
+     * @return void
+     */
+    public function add(Order $order): void
+    {
+        foreach ($this->orders as $order) {
+            if ($order->getId() == $order->getId()) {
+                return;
+            }
+        }
+        $orders[] = $order;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountOrders(): int
+    {
+        return count($this->orders);
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountUsers(): int
+    {
+        $usersStats = [];
+        foreach ($this->orders as $order) {
+            $usersStats[$order->getUser()->getId()] = true;
+        }
+
+        return count($usersStats);
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        $price = 0;
+        foreach ($this->orders as $order) {
+            $price += $order->getLaterPrice();
+        }
+
+        return $price;
+    }
+}
